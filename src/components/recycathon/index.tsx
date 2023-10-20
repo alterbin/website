72a3c4255afb97df03949be6f038e7f024de4436
Kunle Didunyemi <senatorkay2@gmail.com>
2023-10-20 10:19:51 +0100

    rearrange import

--- src/components/recycathon/index.tsx
+++ src/components/recycathon/index.tsx
@@ -1,7 +1,7 @@
-export { default as RecycathonNavbar } from './navbar';
 export { default as AboutEvent } from './about-event';
-export { default as EventAgenda } from './event-agenda';
 export { default as Competition } from './competition';
-export { default as Showcase } from './showcase';
-export { default as Navbar } from './navbar';
+export { default as EventAgenda } from './event-agenda';
 export { default as Hero } from './hero';
+export { default as Navbar } from './navbar';
+export { default as RecycathonNavbar } from './navbar';
+export { default as Showcase } from './showcase';
