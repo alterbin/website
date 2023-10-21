6a60b3721feb746120f69233d889a4d59bd9d380
iobami <ayobamialadenoye@yahoo.com>
2023-10-21 07:47:52 +0100

    updates objectives page

--- src/components/landing/objectives.tsx
+++ src/components/landing/objectives.tsx
@@ -1,12 +1,77 @@
 import Image from 'next/image';
 import assets from '@/utils/assets';
 import { Typography } from '../shared';
+import { Arrow } from '../svgs/arrows';
+
+function ToMinimize() {
+  return (
+    <svg width="62" height="62" viewBox="0 0 62 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clipPath="url(#clip0_374_509)">
+        <path d="M36.1777 29.274H41.3553L38.7665 25.8223L36.1777 29.274ZM36.1777 32.7257H41.3553L38.7665 36.1774L36.1777 32.7257Z" fill="#06382E" />
+        <path d="M21.5084 20.645H26.686V41.3552H21.5084C21.2796 41.3552 21.0601 41.2643 20.8983 41.1025C20.7364 40.9406 20.6455 40.7211 20.6455 40.4923V21.5079C20.6455 21.2791 20.7364 21.0596 20.8983 20.8978C21.0601 20.7359 21.2796 20.645 21.5084 20.645Z" fill="#06382E" />
+        <path d="M28.4111 20.645H33.5887C33.8175 20.645 34.037 20.7359 34.1989 20.8978C34.3607 21.0596 34.4516 21.2791 34.4516 21.5079V40.4923C34.4516 40.7211 34.3607 40.9406 34.1989 41.1025C34.037 41.2643 33.8175 41.3552 33.5887 41.3552H28.4111V20.645Z" fill="#DD9940" />
+      </g>
+      <defs>
+        <clipPath id="clip0_374_509">
+          <rect width="20.7102" height="20.7102" fill="white" transform="translate(20.6455 20.645)" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+function ToProlong() {
+  return (
+    <svg width="62" height="62" viewBox="0 0 62 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clipPath="url(#clip0_374_514)">
+        <path d="M38.7669 41.355H31.0006C30.7718 41.355 30.5523 41.2641 30.3904 41.1023C30.2286 40.9404 30.1377 40.721 30.1377 40.4921V38.7662C30.1377 38.5374 30.2286 38.3179 30.3904 38.1561C30.5523 37.9942 30.7718 37.9033 31.0006 37.9033H38.7669C38.9958 37.9033 39.2153 37.9942 39.3771 38.1561C39.5389 38.3179 39.6299 38.5374 39.6299 38.7662V40.4921C39.6299 40.721 39.5389 40.9404 39.3771 41.1023C39.2153 41.2641 38.9958 41.355 38.7669 41.355Z" fill="#06382E" />
+        <path d="M38.7666 20.645H23.234C23.0052 20.645 22.7857 20.7359 22.6238 20.8978C22.462 21.0596 22.3711 21.2791 22.3711 21.5079V23.2338C22.3711 23.4627 22.462 23.6821 22.6238 23.844C22.7857 24.0058 23.0052 24.0967 23.234 24.0967H38.7666C38.9955 24.0967 39.215 24.0058 39.3768 23.844C39.5387 23.6821 39.6296 23.4627 39.6296 23.2338V21.5079C39.6296 21.2791 39.5387 21.0596 39.3768 20.8978C39.215 20.7359 38.9955 20.645 38.7666 20.645Z" fill="#DD9940" />
+        <path d="M38.7666 25.8223H23.234C23.0052 25.8223 22.7857 25.9132 22.6238 26.075C22.462 26.2368 22.3711 26.4563 22.3711 26.6852V35.3144C22.3711 35.5433 22.462 35.7628 22.6238 35.9246C22.7857 36.0864 23.0052 36.1774 23.234 36.1774H38.7666C38.9955 36.1774 39.215 36.0864 39.3768 35.9246C39.5387 35.7628 39.6296 35.5433 39.6296 35.3144V26.6852C39.6296 26.4563 39.5387 26.2368 39.3768 26.075C39.215 25.9132 38.9955 25.8223 38.7666 25.8223Z" fill="#06382E" />
+      </g>
+      <defs>
+        <clipPath id="clip0_374_514">
+          <rect width="20.7102" height="20.7102" fill="white" transform="translate(20.6455 20.645)" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+function ToRecycle() {
+  return (
+    <svg width="62" height="62" viewBox="0 0 62 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clipPath="url(#clip0_374_537)">
+        <path d="M41.0107 21.6804C40.8381 21.5078 40.493 21.5078 40.2341 21.5078L35.3154 22.9748V40.3196L40.7519 38.7663C41.097 38.68 41.3559 38.3348 41.3559 37.9034V22.3707C41.3559 22.1119 41.2696 21.853 41.0107 21.6804Z" fill="#06382E" />
+        <path d="M33.5887 22.9751L28.4111 21.6807V39.0254L33.5887 40.3198V22.9751Z" fill="#DD9940" />
+        <path d="M26.686 21.6807L21.2496 23.2339C20.9044 23.4065 20.6455 23.7517 20.6455 24.0969V39.6295C20.6455 39.8884 20.7318 40.1472 20.9907 40.3198C21.1633 40.4061 21.3358 40.4924 21.5084 40.4924H21.7673L26.686 39.0254V21.6807Z" fill="#06382E" />
+      </g>
+      <defs>
+        <clipPath id="clip0_374_537">
+          <rect width="20.7102" height="20.7102" fill="white" transform="translate(20.6455 20.645)" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+function ToSensitize() {
+  return (
+    <svg style={{ minWidth: 62 }} width="62" height="62" viewBox="0 0 62 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M28.4122 29.2741H22.3717C22.1429 29.2741 21.9234 29.1832 21.7615 29.0214C21.5997 28.8596 21.5088 28.6401 21.5088 28.4112V22.3707C21.5088 22.1419 21.5997 21.9224 21.7615 21.7606C21.9234 21.5987 22.1429 21.5078 22.3717 21.5078H28.4122C28.641 21.5078 28.8605 21.5987 29.0224 21.7606C29.1842 21.9224 29.2751 22.1419 29.2751 22.3707V28.4112C29.2751 28.6401 29.1842 28.8596 29.0224 29.0214C28.8605 29.1832 28.641 29.2741 28.4122 29.2741Z" fill="#06382E" />
+      <path d="M28.4122 40.4919H22.3717C22.1429 40.4919 21.9234 40.401 21.7615 40.2392C21.5997 40.0773 21.5088 39.8578 21.5088 39.629V33.5885C21.5088 33.3596 21.5997 33.1402 21.7615 32.9783C21.9234 32.8165 22.1429 32.7256 22.3717 32.7256H28.4122C28.641 32.7256 28.8605 32.8165 29.0224 32.9783C29.1842 33.1402 29.2751 33.3596 29.2751 33.5885V39.629C29.2751 39.8578 29.1842 40.0773 29.0224 40.2392C28.8605 40.401 28.641 40.4919 28.4122 40.4919Z" fill="#06382E" />
+      <path d="M31.8643 22.3706H40.4935V24.0965H31.8643V22.3706Z" fill="#DD9940" />
+      <path d="M40.4935 33.5889H31.8643V35.3147H40.4935V33.5889Z" fill="#DD9940" />
+      <path d="M40.4935 37.9033H31.8643V39.6292H40.4935V37.9033Z" fill="#06382E" />
+      <path d="M40.4935 26.6855H31.8643V28.4114H40.4935V26.6855Z" fill="#06382E" />
+    </svg>
+  );
+}
 
 const objs = [
-  { label: 'To minimize waste generation' },
-  { label: 'To prolong usage of items by transferring ownership' },
-  { label: 'To recycle waste into reusable items' },
-  { label: 'To sensitize and educate people about the effect of poor waste management and the importance of good waste management.' },
+  { label: 'To minimize waste generation', icon: <ToMinimize /> },
+  { label: 'To prolong usage of items by transferring ownership', icon: <ToProlong /> },
+  { label: 'To recycle waste into reusable items', icon: <ToRecycle /> },
+  { label: 'To sensitize and educate people about the effect of poor waste management and the importance of good waste management.', icon: <ToSensitize /> },
 ];
 
 export default function Objectives() {
@@ -14,22 +79,22 @@ export default function Objectives() {
     <div className="app_objectives">
       <div className="app_landing_page__px">
         <div className="app_objectives__top">
-          <Typography className="app_mission__top__h2" fontFamily="TrenchSlab" fontWeight="md" variant="h2">
-            Objectives
-          </Typography>
+          <span />
 
-          {/* <a className="app_hero_section__content__txt__action" href="/">
-            <Arrow />
+          {false && (
+            <a className="app_hero_section__content__txt__action" href="/">
+              <Arrow />
 
-            <Typography variant="span">
-              Become a sponsor
-            </Typography>
-          </a> */}
+              <Typography variant="span">
+                Become a sponsor
+              </Typography>
+            </a>
+          )}
         </div>
 
         <div className="app_objectives__main">
           <div className="app_objectives__main__stack">
-            <div className="app_objectives__main__stack__lft">
+            <div className="app_objectives__main__stack__top">
               <Image
                 className="app_objectives__main__stack__img app_objectives__main__stack__img--1"
                 src={assets.objectives.obj1}
@@ -43,21 +108,38 @@ export default function Objectives() {
               />
             </div>
 
-            <Image
-              className="app_objectives__main__stack__img app_objectives__main__stack__img--3"
-              src={assets.objectives.obj3}
-              alt=""
-            />
+            <div className="app_objectives__main__stack__btm">
+              <Image
+                className="app_objectives__main__stack__img"
+                src={assets.objectives.obj3}
+                alt=""
+              />
+
+              <Image
+                className="app_objectives__main__stack__img"
+                src={assets.objectives.obj4}
+                alt=""
+              />
+            </div>
+
           </div>
 
           <div className="app_objectives__main__txt">
-            {objs.map((item, index) => (
-              <div key={item.label}>
-                <Typography className={`app_objectives__main__txt__h3 ${index === 1 ? 'active' : ''}`} variant="h3">
-                  {item.label}
-                </Typography>
+            <Typography className="app_mission__top__h2 text-start" fontFamily="TrenchSlab" fontWeight="md" variant="h2">
+              Objectives
+            </Typography>
 
-                <div className="app_objectives__main__txt__dvdr" />
+            {objs.map((item) => (
+              <div key={item.label}>
+                <div className="d-flex align-items-center">
+                  {item.icon}
+
+                  <Typography className="app_objectives__main__txt__h3 active" variant="h3">
+                    {item.label}
+                  </Typography>
+                </div>
+
+                {false && <div className="app_objectives__main__txt__dvdr" />}
               </div>
             ))}
           </div>
--- src/utils/assets/objectives.ts
+++ src/utils/assets/objectives.ts
@@ -1,7 +1,10 @@
-import obj1 from '../../../public/media/landing/objectives/1.jpg';
-import obj2 from '../../../public/media/landing/objectives/2.jpg';
-import obj3 from '../../../public/media/landing/objectives/3.jpg';
+import obj1 from '../../../public/media/landing/objectives/1.png';
+import obj2 from '../../../public/media/landing/objectives/2.png';
+import obj3 from '../../../public/media/landing/objectives/3.png';
+import obj4 from '../../../public/media/landing/objectives/4.png';
 
-const objectives = { obj1, obj2, obj3 };
+const objectives = {
+  obj1, obj2, obj3, obj4,
+};
 
 export default objectives;
