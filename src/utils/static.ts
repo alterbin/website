fce676a019e02520d87bba120c5f38b4d0422a10
femiayodeji <femidotayodeji@gmail.com>
2025-10-23 05:45:27 +0100

    seo

--- src/utils/static.ts
+++ src/utils/static.ts
@@ -5,10 +5,10 @@ export const metaData = {
   title: 'Alterbin',
   description: 'Alterbin provides creative and innovative projects to transform waste for a suitable future.',
   image: `${config.SITE_URL}/media/landing.png`,
-  keywords: 'Waste disposal, Repurposing, Solid waste management, Waste collection, Environmental waste management, Trash disposal, Waste reduction, Waste management services, Sustainable waste management, Residential waste management, Green waste management, Municipal waste management, Waste management company, Waste management solutions, Waste management and repurposing, Waste management best practices, Waste management guidelines',
+  keywords: 'Waste disposal, Repurposing, Solid waste management, Waste collection, Environmental waste management, Trash disposal, Waste reduction, Waste management services, Sustainable waste management, Residential waste management, Green waste management, Municipal waste management, Waste management company, Waste management solutions, Waste management and repurposing, Waste management best practices, Waste management guidelines, Alterbin',
   url: config.SITE_URL,
   geo: {
-    placeName: 'Lagos',
+    placeName: 'Ibadan, Oyo, Nigeria',
     position: '6.5474147;3.3639306',
     region: 'NG',
   },
